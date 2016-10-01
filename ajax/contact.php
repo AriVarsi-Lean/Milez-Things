b24729acd3538915249977fabeb0fccdfa937d7c
AriVarsi-Lean <varsi.dambaza@leansysystems.co.za>
2016-10-01 18:22:49 +0200

    sorting the mail send

--- ajax/contact.php
+++ ajax/contact.php
@@ -11,11 +11,11 @@ $message = $_POST['message'];
     echo 'Your name  is '.$_POST['name'];*/
 	
 // create email body and send it	
-$to = 'info@ktasolutions.co.za'; // PUT YOUR EMAIL ADDRESS HERE
+$to = 'info@milezauto.co.za'; // PUT YOUR EMAIL ADDRESS HERE
 $email_subject = "From Proffessional Accreds site:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
 $email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@milezauto.co.za\n";
-$headers .= "Reply-To: $info@milezauto.co.za";	
+$headers .= "Reply-To: info@milezauto.co.za";	
 mail($to,$email_subject,$email_body,$headers);
 
 }
